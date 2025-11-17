087f598c2bced73b1a31f862119ecb013698528f
Enel Yusifli <enel1yusifli@gmail.com>
2025-11-17 02:01:28 +0100

    add instances

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def decode_solution(output, n):
     lines = output.splitlines()
     model_line = [l for l in lines if l.startswith("v ")]
     if not model_line:
-        print("No model found.")
+        print("Unsatisfiable")
         return
     values = []
     for l in model_line:
@@ -109,7 +109,7 @@ def decode_solution(output, n):
 if __name__ == "__main__":
     solver_path = "/home/enel/Downloads/glucose/simp/glucose"
     n = 9
-    cages = [
+    cages_satisfliable = [
         (3,  [(0,0),(0,1)]),
         (15, [(0,2),(0,3),(0,4)]),
         (22, [(0,5),(1,5),(1,4),(2,4)]),
@@ -140,8 +140,39 @@ if __name__ == "__main__":
         (13, [(8,4),(8,5),(8,6)]),
         (17, [(8,7),(8,8)]),
     ]
-    clauses = encode_killer_sudoku(n, cages)
+    cages_unsatisfliable = [
+        (3,  [(0,0),(0,1)]),
+        (15, [(0,2),(0,3),(0,4)]),
+        (22, [(0,5),(1,5),(1,4),(2,4)]),
+        (4,  [(0,6),(1,6)]),
+        (16, [(0,7),(1,7)]),
+        (15, [(0,8),(1,8),(2,8),(3,8)]),
+        (25, [(1,0),(1,1),(2,0),(2,1)]),
+        (17, [(1,2),(1,3)]),
+        (9,  [(2,2),(2,3),(3,3)]),
+        (8,  [(2,5),(3,5),(4,5)]),
+        (20, [(2,6),(2,7),(3,6)]),
+        (6,  [(3,0),(4,0)]),
+        (14, [(3,1),(3,7)]), # changed this line
+        (17, [(3,4),(4,4),(5,4)]),
+        (17, [(3,7),(4,6),(4,7)]),
+        (13, [(4,1),(4,2),(5,1)]),
+        (20, [(4,3),(5,3),(6,3)]),
+        (12, [(4,8),(5,8)]),
+        (27, [(5,0),(6,0),(7,0),(8,0)]),
+        (6,  [(5,2),(6,1),(6,2)]),
+        (20, [(5,5),(6,5),(6,6)]),
+        (6,  [(5,6),(5,7)]),
+        (10, [(6,4),(7,4),(7,3),(8,3)]),
+        (14, [(6,7),(6,8),(7,7),(7,8)]),
+        (8,  [(7,1),(8,1)]),
+        (16, [(7,2),(8,2)]),
+        (15, [(7,5),(7,6)]),
+        (13, [(8,4),(8,5),(8,6)]),
+        (17, [(8,7),(8,8)]),
+    ]
+    clauses = encode_killer_sudoku(n, cages_satisfliable)
     write_cnf(clauses, n, "puzzle.cnf")
     output = run_glucose(solver_path, "puzzle.cnf")
-    print(output) 
+    # print(output) 
     decode_solution(output,n)
